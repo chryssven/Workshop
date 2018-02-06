3c58dea8ec49ddee10c5cb093c09c51cc0ed96be
Vence Takayama <35859825+chryssven@users.noreply.github.com>
2018-02-06 11:56:05 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
-# workshop
+<html>
+<head>
+<title>Workshop</title>
+</head>
+<body style="border:2px solid MediumSeaGreen;">
+<div style="background-color:MediumSeaGreen">
+<h1 style="color:white"><center>Personal Information</center></h1>
+</div>
+<p><b>Name:</b> Albores, Chryss Ven James</p>
+<p><b>Nicname:</b> Vence</p>
+<p><b>Age:</b> 21</p>
+<p><b>Sex:</b> Male</p>
+<p><b>Height:</b> 5'6</p>
+<p><b>Weight:</b> 65kg</p>
+<p><b>Nationality:</b> Filipino/Japanese</p>
+
+
+
+<div style="background-color:MediumSeaGreen">
+<h1 style="color:white"><center>Biography</center></h1>
+</div>
+<p>I'm Chryss Ven James Albores and was born on a cold winter night, when even time seemed to stand still, in Cebu City St. Vincent hospital. I do not remember much of my early childhood, 
+but my mom said that I was a very active, curious, and communicative child. I would ask dozens of questions each minute, even without waiting for the answers. 
+I suppose this is why my parents offered me books and educational movies as early as my third birthday.</p>
+<div style="background-color:MediumSeaGreen">
+<p><Center><b><h2 style="color:white">Who am i?</h2></b></center></p>
+</div>
+<p>Fortunately, my thirst for knowledge did not come to an end when I was at school. I was passionate about computer. This passion helped me gain profound 
+knowledge in these areas, and I was admitted to the college of my dreams. Today, I am a student at College Of Technological University, and I feel very happy about it.</p>
+<div style="background-color:MediumSeaGreen">
+<p><Center><b><h2 style="color:white">My goals</h2></b></center></p>
+</div>
+<p>I am certain that my degree will become my ticket to a better tomorrow. I want to become an An IT Specialist, computer professional, or an IT professional may be a person 
+working in the field of information technology. I study hard and devote my free time to reading scholarly reviews and watching interviews with recognized specialists in the field.</p>
+<div style="background-color:MediumSeaGreen">
+<p><Center><b><h2 style="color:white">My ups and downs</h2></b></center></p>
+</div>
+<p>Of course, I understand that life is not just a bed of roses, and challenges and hardships are an integral element of life. Since my parents could not help me cover
+my college expenses in full, paying off my student loan has become an important challenge for me. I combine a part-time job and full-time study to earn my living and 
+my education. I feel triumphant at the beginning of every month when I receive my salary and plan how I will spend my money.</p>
+
+</body>
+</html>
